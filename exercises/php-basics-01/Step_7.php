a78d201c5177dd565e782eeec41bef4fc2f3326d
mohamadalmell <mohamadalmell@gmail.com>
2022-02-24 14:18:33 +0200

    step 8

---
+++ exercises/php-basics-01/Step_7.php
@@ -0,0 +1,10 @@
+<?php
+$birth_year = 1996;
+$current_year = 2022;
+$difference = $current_year - $birth_year;
+if ($difference > 18) {
+    echo "You can drive";
+} else {
+   echo "You still a kid, go and play GTA";
+}
+?>
