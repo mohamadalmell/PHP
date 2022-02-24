9f455c85ac9bc01ae9455736e5bdeb844ec63d9a
mohamadalmell <mohamadalmell@gmail.com>
2022-02-24 12:35:29 +0200

    Step_4

---
+++ exercises/php-basics-01/Step_4.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_erros', 1);
+$name = "Mohamad";
+$position = "Developer";
+$github_url = "https://github.com/mohamadalmell";
+echo "I'm ".$name." a ".$position." please visit my github ".$github_url;
+echo "I'm $name a $position please visit my github $github_url";
+echo 'I\'m '.$name.' a '.$position.' please visit my github '.$github_url;
+echo "I'm {$name} a {$position} please visit my github {$github_url}";
+?>
