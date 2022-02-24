fefb058df790f05487c808335cf52ace8332d337
mohamadalmell <mohamadalmell@gmail.com>
2022-02-24 12:54:48 +0200

    step 5

---
+++ exercises/php-basics-01/Step_5.php
@@ -0,0 +1,9 @@
+<?php
+$course_name = "Python";
+$enrolled_students = 30;
+$price = 79.99;
+$on_discount = true;
+
+echo "Course title: $course_name\nEnrolled Students: $enrolled_students\nCourse Price: $price $ USD\nCourse on discount: ";
+echo  $on_discount ? "Yes\n" : "No\n";
+?>
