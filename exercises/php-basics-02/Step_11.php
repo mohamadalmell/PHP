403d30d49fd24344b7ff5d3bd3a75dec048f2453
mohamadalmell <mohamadalmell@gmail.com>
2022-02-27 17:57:30 +0200

    Basics-02 Step 11

--- exercises/php-basics-02/Step_11.php
+++ exercises/php-basics-02/Step_11.php
@@ -1,3 +1,11 @@
 <?php
-
+function is_Prime($n){
+    for ($i=2; $i <$n ; $i++) { 
+        if ($n % $i == 0) {
+            return "It is not a Prime number";
+        }
+    }
+    return "It is a Prime number";
+}
+print is_Prime(15);
 ?>
