92d5f66e660d3c0c14386fcad3b394332355742d
mohamadalmell <mohamadalmell@gmail.com>
2022-02-28 10:18:40 +0200

    Basics-03 Step 3

--- exercises/php-basics-03/Step_3.php
+++ exercises/php-basics-03/Step_3.php
@@ -1,3 +1,15 @@
 <?php
-
+function counter($text, $word){
+    $arr = explode(" ", $text);
+    $x = 0;
+    $total =0;
+    while ($x < count($arr)) {
+        if ($word == $arr[$x]) {
+            $total++;
+        }
+        $x++;
+    }
+    return $total;
+}
+ print counter("this is it is", "is");
 ?>
