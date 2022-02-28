e0fd557df362a830b3150d749fb51ecbc042cee8
mohamadalmell <mohamadalmell@gmail.com>
2022-02-28 12:53:42 +0200

    Basics-03 Step 8

--- exercises/php-basics-03/Step_8.php
+++ exercises/php-basics-03/Step_8.php
@@ -1,3 +1,21 @@
 <?php
 
+$transactions = array(
+    array(
+        "id" => 1,
+        "debit"=>2,
+        "credit"=>3
+    ),
+    array(
+        "id" => 2,
+        "debit"=>15,
+        "credit"=>10
+    )
+);
+
+foreach ($transactions as $value){
+    $amount = abs($value['debit']-$value['credit']);
+    echo "ID: ".$value['id']." => amount: $amount\n";
+}
+
 ?>
