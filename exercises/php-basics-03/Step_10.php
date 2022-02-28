b07498dc97781da73dd95ecfd653406c30cdb599
mohamadalmell <mohamadalmell@gmail.com>
2022-02-28 14:41:04 +0200

    Basics-03 Step 10

--- exercises/php-basics-03/Step_10.php
+++ exercises/php-basics-03/Step_10.php
@@ -1,3 +1,15 @@
 <?php
+function pers($personal_details){
+    $personal_details = array(
+        "name" => "Rajesh Rao",
+         "occupation" => "Engineer",
+          "age" => 39,
+           "country" => "India"
+    );
+    foreach ( $personal_details as $key => $value ){
+    echo "$key=$value\n";
+    }
+}
 
+print pers("Mohamad");
 ?>
