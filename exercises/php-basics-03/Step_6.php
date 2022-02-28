f5e9afc8ab01c9330e03f11c1fc788cd10d50397
mohamadalmell <mohamadalmell@gmail.com>
2022-02-28 11:55:01 +0200

    Basics-03 Step 6

--- exercises/php-basics-03/Step_6.php
+++ exercises/php-basics-03/Step_6.php
@@ -1,3 +1,14 @@
 <?php
-
+function star($n){
+    for ($i=0; $i <$n ; $i++) { 
+        for ($j=0; $j <$n ; $j++) { 
+            if ($i == 0 || $i == $j || $i == $n-1) {
+                echo "*";
+            }
+            else echo " ";
+        }
+        echo "\n";
+    }
+}
+star(6);
 ?>
