d45ca75fe04b6ffabffbf42343bb46963e844b43
mohamadalmell <mohamadalmell@gmail.com>
2022-02-28 10:54:43 +0200

    Basics-03 Step 4

--- exercises/php-basics-03/Step_4.php
+++ exercises/php-basics-03/Step_4.php
@@ -1,3 +1,11 @@
 <?php
+function computer($num){
+    $counter = 0;
+    for ($i=0; $i <strlen($num) ; $i++) { 
+        $counter += $num[$i];
+    }
+    return $counter;
+}
 
+print computer('14');
 ?>
