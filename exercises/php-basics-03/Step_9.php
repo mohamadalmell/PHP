36c909cc58204fc68eef51e9c7520227ded9b466
mohamadalmell <mohamadalmell@gmail.com>
2022-02-28 14:05:18 +0200

    Basics-03 Step 9

--- exercises/php-basics-03/Step_9.php
+++ exercises/php-basics-03/Step_9.php
@@ -1,3 +1,21 @@
 <?php
+$transactions = array(
+    array(
+        "id" => 1,
+        "name"=> "Gaby",
+        "email"=> "gaby@codi.tech"
+    ),
+    array(
+       "id" => 3,
+       "name"=> "Omar",
+       "email"=> "omar@codi.tech"
+    )
+);
 
+foreach($transactions as $value){
+    $id = $value['id'];
+    $name = $value['name'];
+    $email = $value['email'];
+    echo "id: $id, name: $name, email: $email\n";
+}
 ?>
